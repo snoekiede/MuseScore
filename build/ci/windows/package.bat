1d21d81be8e2fcb7fe09757fbe636664e878df78
Casper Jeukendrup <48658420+cbjeukendrup@users.noreply.github.com>
2023-05-05 23:22:58 +0200

    Copy more info to the artifact

--- build/ci/windows/package.bat
+++ build/ci/windows/package.bat
@@ -160,11 +160,14 @@ MKDIR %ARTIFACTS_DIR%\logs\WIX
 SET WIX_LOG_DIR=win64
 IF %TARGET_PROCESSOR_BITS% == 32 ( SET WIX_LOG_DIR=win32 ) 
 
-SET WIX_LOGS_PATH="%BUILD_DIR%\_CPack_Packages\%WIX_LOG_DIR%\WIX"
-ECHO "Copy from %WIX_LOGS_PATH% to %ARTIFACTS_DIR%\logs\WIX"
+@REM SET WIX_LOGS_PATH="%BUILD_DIR%\_CPack_Packages\%WIX_LOG_DIR%\WIX"
+@REM ECHO "Copy from %WIX_LOGS_PATH% to %ARTIFACTS_DIR%\logs\WIX"
 
-ECHO .msi > excludedmsi.txt
-XCOPY /Y /EXCLUDE:excludedmsi.txt %WIX_LOGS_PATH% %ARTIFACTS_DIR%\logs\WIX
+@REM ECHO .msi > excludedmsi.txt
+@REM XCOPY /Y /EXCLUDE:excludedmsi.txt %WIX_LOGS_PATH% %ARTIFACTS_DIR%\logs\WIX
+
+ECHO "Copy from %BUILD_DIR%\_CPack_Packages to %ARTIFACTS_DIR%\_CPack_Packages"
+XCOPY /Y "%BUILD_DIR%\_CPack_Packages" "%ARTIFACTS_DIR%\_CPack_Packages"
 
 :: find the MSI file without the hardcoded version
 for /r %%i in (%BUILD_DIR%\*.msi) do (
